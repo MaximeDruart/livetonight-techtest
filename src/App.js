e27ad13c0611ed54d66ad9c5a24c6b4a7bb66a64
Maxime Druart <44201023+MaximeDruart@users.noreply.github.com>
2021-05-01 13:53:28 +0200

    :sparkles: cleared up routing

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import { Route, Switch } from "react-router"
 import ArtistPage from "./components/artistPage"
 import Header from "./components/global/Header"
 import Footer from "./components/global/Footer"
+import { Link } from "react-router-dom"
 
 const App = () => {
   return (
     <>
-      <Header />
       <Switch>
         <Route exact path='/'>
-          home
+          <p>homepage</p>
+          <Link to='/groupe-musique-dj/58'> go to Artist page</Link>
         </Route>
         <Route path='/groupe-musique-dj/:name'>
+          {/* normally header would be a global component (on all routes) but to make it easier for the exercice i just put it in the one route i worked on */}
+          <Header />
           <ArtistPage />
         </Route>
         <Route path='/groupe-musique-dj/'>all artists</Route>
-        <Route path='*'>404</Route>
+        <Route path='*'>
+          <p>404</p>
+          <Link to='/'>go to homepage</Link>
+        </Route>
       </Switch>
       <Footer />
     </>
